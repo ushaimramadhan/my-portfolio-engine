2e55ad2890ad7e1542c6f4a2913ca28b3887b991
Ushaim Ramadhan F <adhan1005@gmail.com>
2026-02-03 21:30:31 +0700

    refactor app.js to a new route

--- server/src/app.js
+++ server/src/app.js
@@ -2,20 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const db = require('./config/db');
-
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+const projectRoutes = require('./routes/projectRoutes');
 
 //Middleware
 app.use(cors());
 app.use(express.json()); //Supaya bisa baca JSON dari body request
 
-//Cek Route
+//Routes
 app.get('/', (req, res) => {
   res.send('API Portfolio Engine is Running!');
 });
+
+app.use('/api/projects', projectRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
