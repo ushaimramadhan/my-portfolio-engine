faca2bd91aba989db4357002a93b3c8a9df66f6b
Ushaim Ramadhan F <adhan1005@gmail.com>
2026-02-04 13:54:18 +0700

    implement auth middleware to project route

--- server/src/routes/projectRoutes.js
+++ server/src/routes/projectRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const ProjectController = require('../controllers/projectController');
+const verifyToken = require('../middleware/authMiddleware');
 
 router.get('/', ProjectController.index); // GET http://localhost:5000/api/projects
-router.post('/', ProjectController.store); // POST http://localhost:5000/api/projects
+router.post('/', verifyToken, ProjectController.store); // POST http://localhost:5000/api/projects
 
 module.exports = router;
